3f29651071277b2a5e4aa03a4e82464c8a045357
Keith Vassallo <keith@icemalta.com>
2025-07-15 09:08:11 +0100

    Migrated to Vite. Changed API to LanguageTool.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useId, useState, useEffect, memo } from 'react';
 import './App.css';
 
-const apiKey = process.env.REACT_APP_BING_API_KEY;
-const apiEndpoint = process.env.REACT_APP_BING_API_ENDPOINT;
-
-
 function App() {
   return <div className='container'>
     <div className='rounded-container'>
